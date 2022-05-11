9db435cd52e0f831ed8d054ab5d859e144d39a6d
Kashmal Rahat <kashmalch1@gmail.com>
2022-05-11 19:24:32 +0500

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,5 +1,2 @@
-SELECT
-    first_name AS name,
-    variant_id AS id,
-    category AS brand
-FROM demo_dataset.demo_table
+SELECT first_name AS name, variant_id AS id,
+category AS brand FROM demo_dataset.demo_table
